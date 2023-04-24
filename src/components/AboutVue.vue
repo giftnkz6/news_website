7903c82d15d7ac8ca5d410a0006d15139462a0a7
Gift Mahlangu <giftnkz6@gmail.com>
2023-04-24 07:43:53 +0200

    Added an asset

--- src/components/AboutVue.vue
+++ src/components/AboutVue.vue
@@ -2,7 +2,8 @@
     <div class="about">
         <h1 id="about">ABOUT US</h1>
         <div class="container">
-            <img src="https://cdn.dribbble.com/users/3873964/screenshots/14523057/media/02a1ca5dc4e5faacfac8e754195b118c.gif" alt="online news">
+            <img src="../assets/output-onlinegiftools.gif" alt="">
+            <!-- <img src="https://cdn.dribbble.com/users/3873964/screenshots/14523057/media/02a1ca5dc4e5faacfac8e754195b118c.gif" alt="online news"> -->
             <div class="about-text">
                 <h2>OUR MISSION</h2>
                 <p>Our goal is to provide the best and factual stories to our clients at a click of a button. Gone are the days where in order to stay up to date you needed to drive to a shop to buy a newspaper. Now we are in a digital world where we bring you, your favorite stories at the comfort of your home.
--- src/components/HomeVue.vue
+++ src/components/HomeVue.vue
@@ -3,7 +3,7 @@
       <h1>GET THE TRENDING ARTICLES</h1>
       <img src="https://i.postimg.cc/YSGHFTNW/Screenshot-6-removebg-preview.png" alt="">
       <div class="deco"></div>
-      <div class="deco1"></div>
+      <!-- <div class="deco1"></div> -->
     </div>
   
     <!-- <button class="btn btn-info">VIEW ARTICLES</button> -->
@@ -50,19 +50,99 @@
   
     .deco{
       position: absolute;
-      width: 100px;
-      height: 80px;
+      width: 300px;
+      height: 300px;
       background-color: #289DFF;
-      margin-left: 36rem;
-      border-radius: 40% 95% 80% / 80% 40% 15%;
+      margin-left: 15rem;
+      /* margin-top: 3rem; */
+      border-radius: 100% 67% 100% 60% / 190% 65% 40% 50%;
+      opacity: 35%;
     }
     .deco1{
       position: absolute;
-      width: 100px;
-      height: 100px;
+      width: 200px;
+      height: 200px;
       background-color: #289DFF;
-      margin-left: 70rem;
+      margin-left: 50rem;
       margin-top: 9rem;
-      border-radius: 60% 15% 80% / 80% 40% 15%;
+      border-radius: 60% 35% 80% 200%/ 80% 40% 15% 135%;
+      opacity: 35%;
+    }
+
+    @media screen and (max-width: 970px){
+      .home{
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      margin-top: 6rem;
+      margin-bottom: 16rem;
+      margin: auto;
+      
+  
+    }
+    img{
+      margin: auto;
+    }
+
+    h1{
+      text-align: center;
+    }
+
+    }
+
+    @media screen and (max-width: 550px){
+      .home{
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      margin-top: 6rem;
+      margin-bottom: 16rem;
+      margin: auto;
+      
+  
+    }
+
+    img{
+      margin: auto;
+    }
+
+    h1{
+      text-align: center;
+      font-size: 2rem;
+      width: 20rem;
+    }
+
+    .deco{
+      display: none;
+    }
+
+    }
+
+    @media screen and (max-width: 400px){
+      .home{
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      margin-top: 6rem;
+      margin-bottom: 16rem;
+      margin: auto;
+      
+  
+    }
+
+    img{
+      width: 14rem;
+      margin-left: 1rem;
+      margin: auto;
+    }
+
+    h1{
+      text-align: center;
+      font-size: 2rem;
+      width: 18rem;
+      margin-left: 0rem;
+    }
+
+    .deco{
+      display: none;
+    }
+
     }
   </style>
